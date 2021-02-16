725a1c4996fe8b15822822b9666b9d5b123b0c7e
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-16 03:03:56 +0000

    delivered

--- src/main.js
+++ src/main.js
@@ -6,5 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document
+    .querySelector(".card")
+    .classList.add("aqui va la funcion q retorna los tipos de carta");
+  document.querySelector(".card").innerHTML =
+    "aqui va la funcion q retorna el numero de carta";
 };
+
+//funcion para obtener numero carta
+
+let generateRamdonNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+//funcion para obtener tipos de carta
+
+let generateRandomSuit = () => {
+    let suit = ["diammon","heart","spade","club"]
+}
